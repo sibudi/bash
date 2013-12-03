50c961223423cc37c3b2b934748bc6b9efad6963
Setiyabudi <hello@sibudi.net>
2013-12-03 11:34:59 +0700

    edit sed-nya temporary dulu

--- mssqltopgsql.sh
+++ mssqltopgsql.sh
@@ -10,6 +10,13 @@
 
 for f in `ls *.sql`;
 do
+    sed -e ':redo' -e 's/^\([^]]*\[[^] ]*\) /\1_/' -e 't redo' $f > "tmp_$f"
+    sed 's/^[a-zA-Z0-9]/\_/g' "tmp_$f" > "tmp__$f"
+done
+
+for f in `ls tmp__*`;
+do
     sed -f mssqltopgsqlsed.txt $f > "pgsql_$f"
 done
 
+rm tmp*
