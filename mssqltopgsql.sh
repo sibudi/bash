a9b9347140619db83d8957f8f4cef744e8f4faf1
Setiyabudi <hello@sibudi.net>
2013-12-04 09:23:44 +0700

    fin

--- mssqltopgsql.sh
+++ mssqltopgsql.sh
@@ -4,16 +4,24 @@
 # versi 2
 #
 
+#
 # Usage: 
 # versi 1: ./mssqltopgsql.sh <mssql script name> > <pgsql script name>
 # versi 2: ./mssqltopgsql.sh
+#
 
 for f in `ls *.sql`;
 do
-    # hapus komentar dulu
+    # hapus komentar
     sed '/\/\*/d' $f > "tmp_$f"
+
+    # ganti spasi dengan underscore
     sed -e ':redo' -e 's/^\([^]]*\[[^] ]*\) /\1_/' -e 't redo' "tmp_$f" > "tmp__$f"
+    
+    # ganti garis miring dengan underscore
     sed 's/\//\_/g' "tmp__$f" > "tmp___$f"
+
+    # jalankan operasi sed yang lain
     sed -f mssqltopgsqlsed.txt "tmp___$f" > "pgsql_$f"
 done
 
